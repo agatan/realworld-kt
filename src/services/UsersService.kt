49ab37d2ed8d11a1fc3bd2c15057f09c501308ee
agatan <agatan039@gmail.com>
2019-08-12 23:10:06 +0900

    Format

--- src/services/UsersService.kt
+++ src/services/UsersService.kt
@@ -6,11 +6,13 @@ class UsersService {
   private val usersStore = mutableListOf<User>()
 
   fun getUsers(): List<User> = usersStore.toList()
+
   fun createUser(name: String, email: String): User {
     val newUser = User(UserId(usersStore.size), name, email)
     usersStore.add(newUser)
     return newUser
   }
+
   fun getUser(id: UserId): User? {
     return usersStore.find { it.id == id }
   }
