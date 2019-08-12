82d0287dd2ca2819164c2a0536faa8e129bcc4de
agatan <agatan039@gmail.com>
2019-08-12 18:58:30 +0900

    Add interfaces package

--- src/controllers/UserController.kt
+++ src/controllers/UserController.kt
@@ -1,19 +1,17 @@
 package dev.agatan.controllers
 
+import dev.agatan.interfaces.*
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.locations.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-
-data class User(val id: Int, val name: String)
-
 fun Application.usersRoute() {
   routing {
     route("/users") {
       get("/") {
-        val users = (1 until 10).toList().map { User(it, it.toString())}
+        val users = (1 until 10).toList().map { User(it, it.toString()) }
         call.respond(mapOf("users" to users))
       }
 
---
+++ src/interfaces/User.kt
@@ -0,0 +1,3 @@
+package dev.agatan.interfaces
+
+data class User(val id: Int, val name: String)
